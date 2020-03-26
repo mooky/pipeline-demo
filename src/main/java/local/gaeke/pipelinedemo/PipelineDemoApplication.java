9ea981f34665d493dbc5e838a55683cabdf7c196
Steve Gaeke <steve.gaeke@kroger.com>
2020-03-26 13:56:32 -0400

    Add endpoint and minor pipeline refactor.

--- src/main/java/local/gaeke/pipelinedemo/PipelineDemoApplication.java
+++ src/main/java/local/gaeke/pipelinedemo/PipelineDemoApplication.java
@@ -2,7 +2,10 @@ package local.gaeke.pipelinedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class PipelineDemoApplication {
 
@@ -10,4 +13,9 @@ public class PipelineDemoApplication {
         SpringApplication.run(PipelineDemoApplication.class, args);
     }
 
+
+    @GetMapping
+    String getGreeting() {
+        return "Hello, world";
+    }
 }
